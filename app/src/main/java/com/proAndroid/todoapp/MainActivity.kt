b26c72662c05bc51d78ef1f7367e29da87886117
Nirmal Nepal <rockink_nirmal@live.com>
2020-05-23 22:44:33 -0400

    step6-action-listener: added click listeners; referenced views defined in layout

--- app/src/main/java/com/proAndroid/todoapp/MainActivity.kt
+++ app/src/main/java/com/proAndroid/todoapp/MainActivity.kt
@@ -2,11 +2,22 @@ package com.proAndroid.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val todoTitleOriginalText = todoTitle.text
+
+        todoTitle.setOnClickListener {
+            appTitle.text = "TodoTitle clicked!!"
+        }
+
+        todoImage.setOnClickListener {
+            appTitle.text = todoTitleOriginalText
+        }
     }
 }
