547e812ea9b328c3444e1392b8f342923b38463e
Nirmal Nepal <rockink_nirmal@live.com>
2020-05-25 21:34:01 -0400

    refactoring the classes in different files

--- app/src/main/java/com/proAndroid/todoapp/MainActivity.kt
+++ app/src/main/java/com/proAndroid/todoapp/MainActivity.kt
@@ -1,21 +1,9 @@
 package com.proAndroid.todoapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.annotation.DrawableRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.todo_layout.view.*
-
-data class Todo(
-    val title: String,
-    val todoListItem: String,
-    @DrawableRes val imageResource : Int
-)
 
 class MainActivity : AppCompatActivity() {
     private val todo =  Todo(
@@ -29,16 +17,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         updateUi(todoToDisplayList)
-
     }
 
     private fun updateUi(todoList: MutableList<Todo>) {
         val todoRecyclerAdapter = TodoListDisplayAdapter(todoList)
         todoRecyclerView.adapter = todoRecyclerAdapter
         todoRecyclerView.layoutManager = LinearLayoutManager(this)
-
 
         addButton.setOnClickListener {
             todoRecyclerAdapter.updateListWithItem(
@@ -48,45 +33,4 @@ class MainActivity : AppCompatActivity() {
             )
           }
     }
-}
-
-
-class TodoListDisplayAdapter(private val todoList: MutableList<Todo>) :
-    RecyclerView.Adapter<TodoListDisplayAdapter.ViewHolder>() {
-    class ViewHolder(mView: View) : RecyclerView.ViewHolder(mView) {
-        fun updateUi(todo: Todo) {
-            itemView.todoTitle.text = todo.title
-            itemView.todoListItem.text = todo.todoListItem
-            itemView.todoImage.setImageResource(todo.imageResource)
-
-            val todoTitleOriginalText = itemView.todoTitle.text
-
-            itemView.todoTitle.setOnClickListener {
-                itemView.todoTitle.text = "TodoTitle clicked!!"
-            }
-
-            itemView.todoImage.setOnClickListener {
-                itemView.todoTitle.text = todoTitleOriginalText
-            }
-        }
-    }
-
-    fun updateListWithItem(todo: Todo) {
-        todoList.add(todo)
-        notifyItemChanged(todoList.size - 1) // last item
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.todo_layout, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun getItemCount(): Int {
-        return todoList.size
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.updateUi(todoList[position])
-    }
-
 }
