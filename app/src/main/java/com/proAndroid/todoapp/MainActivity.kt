bb986144280c61cf6d8b6ad8ed377d07026bf7b2
Nirmal Nepal <rockink_nirmal@live.com>
2020-05-27 16:19:06 -0400

    added viewModel in the activity

--- app/src/main/java/com/proAndroid/todoapp/MainActivity.kt
+++ app/src/main/java/com/proAndroid/todoapp/MainActivity.kt
@@ -1,23 +1,39 @@
 package com.proAndroid.todoapp
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+
+class TodoViewModel: ViewModel() {
     private val todo =  Todo(
         title = "ProgrammingTodo",
         todoListItem = "Expand our todo App",
         imageResource = R.drawable.programming_image
     )
 
-    private val todoToDisplayList = mutableListOf<Todo>(todo)
+    private val _todoToDisplayList = mutableListOf<Todo>(todo)
+    val todoToDisplayList : MutableList<Todo>
+        get() = _todoToDisplayList.toMutableList()
+
+
+    fun addItem(todo: Todo) {
+        _todoToDisplayList.add(todo)
+    }
+
+}
+
+class MainActivity : AppCompatActivity() {
+
+    private val todoViewModel by viewModels<TodoViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        updateUi(todoToDisplayList)
+        updateUi(todoViewModel.todoToDisplayList)
     }
 
     private fun updateUi(todoList: MutableList<Todo>) {
@@ -26,11 +42,10 @@ class MainActivity : AppCompatActivity() {
         todoRecyclerView.layoutManager = LinearLayoutManager(this)
 
         addButton.setOnClickListener {
-            todoRecyclerAdapter.updateListWithItem(
-                todo.copy(
-                    title = todo.title + System.currentTimeMillis()
-                )
+            todoViewModel.addItem(
+                todoList.first().copy(title = todoList.first().title + System.currentTimeMillis())
             )
+            todoRecyclerAdapter.updateListWithItem(todoViewModel.todoToDisplayList)
           }
     }
 }
