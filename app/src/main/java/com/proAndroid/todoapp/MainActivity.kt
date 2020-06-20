6ba89c6ee3662dd655c1bc4001d3d6d65f6db47b
Nirmal Nepal <rockink_nirmal@live.com>
2020-06-20 09:31:58 -0400

    handling intent with single instance of activity

--- app/src/main/java/com/proAndroid/todoapp/MainActivity.kt
+++ app/src/main/java/com/proAndroid/todoapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.proAndroid.todoapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -10,12 +11,19 @@ import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(this.javaClass.canonicalName, "onCreate called...")
         setContentView(R.layout.main_layout)
 
         //could be created by notification as well!
+        handleNotificationAppStarting(intent)
+    }
+
+    private fun handleNotificationAppStarting(intent: Intent?) {
+        intent?:return
         val notificationActionType = intent.getStringExtra(AppNotification.Actions.ACTION_TYPE_KEY)?:return
         if (notificationActionType == AppNotification.Actions.Delete.ACTION_TYPE) {
             lifecycleScope.launch(Dispatchers.Default) {
@@ -25,6 +33,12 @@ class MainActivity : AppCompatActivity() {
                     .deleteFirstTodo()
             }
         }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Log.d(TAG, "onNewIntent: intent Received")
+        handleNotificationAppStarting(intent)
     }
 }
 
