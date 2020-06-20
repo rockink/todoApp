c49f7077c6ab64ffe18c9d126e801d64c76c50c7
Nirmal Nepal <rockink_nirmal@live.com>
2020-06-19 22:44:45 -0400

    adding notification to the app

---
+++ app/src/main/java/com/proAndroid/todoapp/notification/AppNotification.kt
@@ -0,0 +1,61 @@
+package com.proAndroid.todoapp.notification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_CANCEL_CURRENT
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.proAndroid.todoapp.MainActivity
+import com.proAndroid.todoapp.R
+import com.proAndroid.todoapp.ui.models.Todo
+
+/**
+ * Handles all sorts of notification for the application
+ */
+class AppNotification(private val notificationManager: NotificationManagerCompat, private val context: Context) {
+
+    val NOTIFICATION_ID = 1
+    val CHANNEL_ID = "TodoDisplayChannel"
+
+    companion object {
+        val NOTIFICATION_PRESSED_REQUEST_CODE = 1
+    }
+
+    fun notifyTodosSelected(todoList: List<Todo>) {
+        createNotificationChannel()
+        val notificatioNBuilder = NotificationCompat.Builder(context, CHANNEL_ID)
+
+        notificatioNBuilder
+            .setSmallIcon(R.drawable.programming_image)
+            .setContentTitle("TotalTodos: ${todoList.count()}")
+            .setContentText("Click here to see the todos of your app")
+            .setContentIntent(PendingIntent.getActivity(context, NOTIFICATION_PRESSED_REQUEST_CODE, Intent(context, MainActivity::class.java), FLAG_CANCEL_CURRENT))
+
+        notificationManager.notify(
+            NOTIFICATION_ID,
+            notificatioNBuilder.build()
+        )
+    }
+
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = CHANNEL_ID
+            val descriptionText = "Simple Todo"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
+}
--- app/src/main/java/com/proAndroid/todoapp/ui/todoDisplay/TodoDisplayFragment.kt
+++ app/src/main/java/com/proAndroid/todoapp/ui/todoDisplay/TodoDisplayFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.proAndroid.todoapp.R
 import com.proAndroid.todoapp.asTodoApplication
+import com.proAndroid.todoapp.notification.AppNotification
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
@@ -39,6 +41,11 @@ class TodoDisplayFragment : Fragment() {
         todoViewModel.todoLiveData
             .observe(viewLifecycleOwner, Observer { todoList ->
                 todoRecyclerAdapter.updateListWithItem(todoList)
+
+                view.notifyButton.setOnClickListener {
+                    AppNotification(NotificationManagerCompat.from(requireContext()), requireContext())
+                        .notifyTodosSelected(todoList)
+                }
             })
 
         addButton.setOnClickListener {
@@ -46,6 +53,7 @@ class TodoDisplayFragment : Fragment() {
             findNavController()
                 .navigate(R.id.action_todoDisplayFragment_to_todoAddFragment)
         }
+
     }
 
     companion object {
